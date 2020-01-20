26dc5af9677644777942eb2ced5f47dc837b647d
bnwlkr <bnwlkr@icloud.com>
2020-01-19 23:08:20 -0800

    Add latest version to wguard.rb

--- wguard.rb
+++ wguard.rb
@@ -1,9 +1,8 @@
 class Wguard < Formula
   desc "Easily Block Websites on macOS"
   homepage "https://github.com/bnwlkr/wguard"
-  url "https://github.com/bnwlkr/wguard/archive/v0.1.3.tar.gz"
-  sha256 "77dc8802e6ce9285e4dfb7e68bff54993b18195a335e56c42b84f53cbd016626"
-
+  url "https://github.com/bnwlkr/wguard/archive/v0.1.4.tar.gz"
+  sha256 "7560ae8ac9350274be9b8b80fcb2e400a7508826bd6b6adabf5b16ba8cf39087"
 
   def install
     bin.install "wguard.py" => "wguard"
