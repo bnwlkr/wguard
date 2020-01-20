1a098dfaadfb41ac71bb453c7738db11bc155b0f
bnwlkr <bnwlkr@icloud.com>
2020-01-19 19:26:02 -0800

    add homebrew formula back to the repo

---
+++ wguard.rb
@@ -0,0 +1,15 @@
+class Wguard < Formula
+  desc "Easily Block Websites on Mac"
+  homepage "https://github.com/bnwlkr/wguard"
+  url "https://github.com/bnwlkr/wguard/archive/v0.1.1.tar.gz"
+  sha256 "e478c1d5fdd4cbfc0632bc09e07dd9b44b37c7d9d76e51e12758d9f301644c54"
+
+
+  def install
+    bin.install "wguard.py" => "wguard"
+  end
+
+  test do
+    system "false"
+  end
+end
